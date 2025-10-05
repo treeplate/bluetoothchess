078ade5604dee13a0c678a0947c807c366e73836
treeplate <tree.plate.moo@gmail.com>
2025-10-05 11:54:06 -0700

    move chessboard into its own file

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,8 @@ import 'package:flutter/services.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:universal_ble/universal_ble.dart';
 
+import 'chessboard.dart';
+
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   try {
@@ -117,105 +119,6 @@ class _ServerAppState extends State<ServerApp> {
         setState(() {
           position = position.play(move);
         });
-      },
-    );
-  }
-}
-
-class Chessboard extends StatefulWidget {
-  const Chessboard({super.key, required this.position, required this.move});
-  final Position position;
-  final void Function(Move move) move;
-
-  @override
-  State<Chessboard> createState() => _ChessboardState();
-}
-
-class _ChessboardState extends State<Chessboard> {
-  Square? movedSquare;
-  Offset? target;
-  static const double squareSize = 40;
-  @override
-  Widget build(BuildContext context) {
-    return DragTarget<Square>(
-      onWillAcceptWithDetails: (details) {
-        movedSquare = details.data;
-        return true;
-      },
-      onLeave: (data) {
-        setState(() {
-          target = null;
-        });
-      },
-      builder: (BuildContext context, _, _) {
-        return Listener(
-          onPointerDown: (PointerDownEvent event) {
-            setState(() {
-              target = event.position;
-            });
-          },
-          onPointerUp: (event) {
-            if (target == null || movedSquare == null) return;
-            setState(() {
-              if (!Size(squareSize * 8, squareSize * 8).contains(target!)) {
-                target = null;
-                movedSquare = null;
-                return;
-              }
-              Square square = Square.fromCoords(
-                File.values[target!.dx ~/ squareSize],
-                Rank.values[8 - target!.dy ~/ squareSize],
-              );
-              Move move = NormalMove(from: movedSquare!, to: square);
-              if (widget.position.isLegal(move)) {
-                widget.move(move);
-              }
-              target = null;
-              movedSquare = null;
-            });
-          },
-          onPointerMove: (PointerMoveEvent event) {
-            setState(() {
-              target = event.position;
-            });
-          },
-          child: SizedBox(
-            width: squareSize * 8,
-            height: squareSize * 8,
-            child: Stack(
-              children: [
-                ...widget.position.board.pieces.map(
-                  (e) => Positioned(
-                    left: e.$1.file.value * squareSize,
-                    top: (8 - e.$1.rank.value) * squareSize,
-                    child: Draggable<Square>(
-                      data: e.$1,
-                      feedback: Text(
-                        e.$2.fenChar,
-                        style: TextStyle(fontSize: squareSize),
-                      ),
-                      childWhenDragging: SizedBox.square(dimension: squareSize),
-                      child: Text(
-                        e.$2.fenChar,
-                        style: TextStyle(fontSize: squareSize),
-                      ),
-                    ),
-                  ),
-                ),
-                if (target != null)
-                  Positioned(
-                    left: target!.dx ~/ squareSize * squareSize,
-                    top: target!.dy ~/ squareSize * squareSize,
-                    child: Container(
-                      width: squareSize,
-                      height: squareSize,
-                      color: Colors.grey,
-                    ),
-                  ),
-              ],
-            ),
-          ),
-        );
       },
     );
   }
